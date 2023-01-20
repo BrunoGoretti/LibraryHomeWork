aa1bd6e19576e88a4a16000898d6e4dceefc33e6
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2023-01-20 19:18:13 +0400

    Init

---
+++ Bookrental/Controllers/RentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookrental.Controllers
+{
+    public class RentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
