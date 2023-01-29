c1a5a2f8e29ac58dc73959d1d3804d035157d1f8
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2023-01-30 00:22:42 +0400

    init

---
+++ Bookrental/Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using Bookrental.Data;
+using Bookrental.Models;
+using Bookrental.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookrental.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly ApiContext _context;
+
+        public BookService(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ActionResult<List<BookModel>>> AddBook(string bookName)
+        {
+            var books = new BookModel { BookName = bookName};
+            _context.DbBook.Add(books);
+            _context.SaveChanges();
+            return Ok( _context.DbBook.Where(x => x.BookName == bookName).FirstOrDefaultAsync());
+        }
+
+        public Task<ActionResult<List<BookModel>>> GetBook(int bookId)
+        {
+            var book = _context.DbBook.Find(bookId);
+
+            if (book == null)
+            {
+                return BadRequest("Book not found.");
+            }
+
+            var customerId =  _context.BookModel
+                .Where(x => x.BookId == bookId)
+                .Select(x => x.RentedDetails)
+                .FirstOrDefaultAsync();
+
+            return Ok(book);
+        }
+    }
+}
---
+++ Bookrental/Services/Interfaces/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using Bookrental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookrental.Services.Interfaces
+{
+    public interface IBookService
+    {
+        public Task<ActionResult<List<BookModel>>> AddBook(string bookName);
+    }
+}
